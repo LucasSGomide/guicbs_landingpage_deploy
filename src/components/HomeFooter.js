162695b78d0f49da05b6c7b0479c078c7861eca0
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2021-02-19 19:24:55 -0300

    Atualizando link para whatsapp e pronto para primeira avaliacao

--- src/components/HomeFooter.js
+++ src/components/HomeFooter.js
@@ -7,9 +7,15 @@ import '../styles/homeFooter.css';
 function HomeFooter() {
   return (
     <footer className="home-footer">
+      <a target="_blank" rel="noreferrer" href="https://api.whatsapp.com/send?phone=5531986540018" >
       <img src={ whatsLogo } alt="Link para o whatsapp" className="img-footer" />
+      </a>
+      <a>
       <img src={ TelegramLogo } alt="Link para o telegram" className="img-footer" />
+      </a>
+      <a>
       <img src={ Email } alt="Link para enviar email" className="img-footer" />
+      </a>
     </footer>
   );
 }
